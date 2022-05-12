4e3f8dd706eb750b8b36bf4cb1384945756dfce5
Ierotheos10 <105287381+Ierotheos10@users.noreply.github.com>
2022-05-12 20:51:03 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showNumber(randint(0, 100))
+    basic.showNumber(randint(1, 10))
 })
 basic.showString("love meter")
 basic.forever(function () {
