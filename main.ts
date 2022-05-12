f85a0da351ef6c29090e99663664b43f5efd491b
Ierotheos10 <105287381+Ierotheos10@users.noreply.github.com>
2022-05-12 10:58:33 +0300

    Ενημέρωση main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showNumber(randint(0, 100000))
+    basic.showNumber(randint(0, 100))
 })
 basic.showString("love meter")
 basic.forever(function () {
